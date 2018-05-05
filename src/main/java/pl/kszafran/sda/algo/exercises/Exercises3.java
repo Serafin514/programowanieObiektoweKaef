49d63cb8b199634449e68630b8b975667fae5efe
Krzysztof Kaczynski <Serafin514@gmail.com>
2018-05-05 15:20:51 +0200

    task one complited

--- src/main/java/pl/kszafran/sda/algo/exercises/Exercises3.java
+++ src/main/java/pl/kszafran/sda/algo/exercises/Exercises3.java
@@ -2,8 +2,9 @@ package pl.kszafran.sda.algo.exercises;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -12,21 +13,37 @@ public class Exercises3 {
     /**
      * Znajduje wszystkie pliki o nazwie pasującej do podanego wyrażenia regularnego
      * w podanym katalogu i wszystkich jego podkatalogach.
-     *
      * Zwrócone pliki są posortowane alfabetycznie po pełnej ścieżce (nie tylko nazwie pliku).
-     *
      * Jeśli directory wskazuje na plik, zwrócona lista ma zawierać tylko ten plik
      * (jeśli jego nazwa pasuje do wyrażenia regularnego).
-     *
      * Uwaga: należy skupić się na klasach z pakietu java.io.
      */
-    public List<File> findFiles(File directory, String regex) throws IOException  {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public List<File> findFiles(File directory, String regex) throws IOException {
+        List<File> result = new ArrayList<>();
+        File[] files = directory.listFiles();
+        if (directory.isFile()&& directory.getName().matches(regex)){// do ogarniecia
+            result.add(directory);
+        }
+
+        if (files != null) {//do ogarniecia
+
+            for (File file : files) {
+
+                if (file.isFile() && file.getName().matches(regex)) {
+                    result.add(file);
+
+                } else if (file.isDirectory()) {
+                    result.addAll(findFiles(file, regex));
+                }
+            }
+        }
+        return result;
     }
+
 
     /**
      * Działa tak samo jak findFiles().
-     *
+     * <p>
      * Uwaga: należy korzystac z dobrodziejstw pakietu java.nio.file.
      */
     public Stream<Path> findFilesNIO(Path directory, Pattern regex) throws IOException {
