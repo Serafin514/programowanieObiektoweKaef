c0903a75cf9ac05c11287448753c43e0a4421682
Krzysztof Kaczynski <Serafin514@gmail.com>
2018-05-12 13:22:31 +0200

    działa do peek

--- src/main/java/pl/kszafran/sda/algo/exercises/Exercises5.java
+++ src/main/java/pl/kszafran/sda/algo/exercises/Exercises5.java
@@ -1,5 +1,10 @@
 package pl.kszafran.sda.algo.exercises;
 
+import com.google.common.collect.ObjectArrays;
+
+import java.sql.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.NoSuchElementException;
 
@@ -8,46 +13,97 @@ public class Exercises5 {
     /**
      * Funkcja przyjmuje ciąg znaków zawierający nawiasy okrągłe: "(", ")",
      * a następnie zwraca true jeśli nawiasy są prawidłowo zagnieżdżone.
-     *
+     * <p>
      * Inne znaki są ignorowane.
-     *
+     * <p>
      * Np. dla "(())" zwraca true, ale dla "())(" zwraca false.
      */
     public boolean balancedParens(String input) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        Character charOne = '(';
+        ArrayDeque<Character> arrayDeque1 = new ArrayDeque<>();
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == charOne) {
+                arrayDeque1.push(charOne);
+            } else if (chars[i] == ')') {
+                if (arrayDeque1.isEmpty()) {
+                    return false;
+                }
+                arrayDeque1.pop();
+            }
+        }
+        return arrayDeque1.isEmpty();
+        //throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
      * Funkcja działa analogicznie do balancedParens, ale sprawdza także
      * poprawne zagnieżdzenie nawiasów kwadratowych "[", "]" oraz klamrowych "{", "}".
-     *
+     * <p>
      * Np. dla "[(){}]" zwraca true, ale dla "[(])" zwraca false.
      */
     public boolean balancedAnyParens(String input) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+
+        ArrayDeque<Character> arrayDeque1 = new ArrayDeque<>();
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(') {
+                arrayDeque1.push('(');
+            } else if (chars[i] == ')') {
+                if (arrayDeque1.isEmpty()) {
+                    return false;
+                } else if (arrayDeque1.peek() == '(')
+                    arrayDeque1.pop();
+            }
+
+            if (chars[i] == '{') {
+                arrayDeque1.push('{');
+            } else if (chars[i] == '}') {
+                if (arrayDeque1.isEmpty()) {
+                    return false;
+                } else if (arrayDeque1.peek() == '{')
+                    arrayDeque1.pop();
+            }
+
+            if (chars[i] == '[') {
+                arrayDeque1.push('[');
+            } else if (chars[i] == ']') {
+                if (arrayDeque1.isEmpty()) {
+                    return false;
+                } else if (arrayDeque1.peek() == '[')
+                    arrayDeque1.pop();
+            }
+        }
+        return arrayDeque1.isEmpty();
     }
 
     /**
      * Funkcja odwraca podaną kolejkę.
-     *
+     * <p>
      * Uwaga: wolno używać jedynie metod poll(), offer(), peek() i isEmpty()
      * oraz nie wolno tworzyć innych kolekcji.
      */
     public <T> void reverseQueue(Deque<T> queue) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (queue.size() > 0) {
+            T elementTemp = queue.poll();
+            reverseQueue(queue);          //do przestudiowania
+            queue.offer(elementTemp);
+        }
     }
 
     /**
      * Tworzy nową kolejkę o stałej pojemności 'capacity', zawierającą podane elementy.
-     *
+     * <p>
      * Uwaga: kolejkę należy zaimplementować w oparciu o bufor cykliczny.
      * Opis do znalezienia na Wikipedii: https://pl.wikipedia.org/wiki/Bufor_cykliczny
-     *
+     * <p>
      * Uwaga: najłatwiej będzie implementować metody w takiej kolejności jak są zadeklarowane.
-     *
+     * <p>
      * Podpowiedź: są dwa podstawowe sposoby na zaimplementowanie bufora cyklicznego:
-     *  - przechowywać dwa wskaźniki: na początek i na konieć kolejki
-     *  - przechowywać wskaźnik na początek kolejki oraz ilość elementów (wg mnie prostszy sposób)
+     * - przechowywać dwa wskaźniki: na początek i na konieć kolejki
+     * - przechowywać wskaźnik na początek kolejki oraz ilość elementów (wg mnie prostszy sposób)
      * Dla zainteresowanych tematem: https://www.snellman.net/blog/archive/2016-12-13-ring-buffers/
      *
      * @throws IllegalArgumentException jeśli ilość elementów przekracza pojemność
@@ -97,27 +153,39 @@ public class Exercises5 {
 
     private static class SdaCircularBuffer<T> implements SdaQueue<T> {
 
+
+        private T[] elements;
+        private int size;
+
+
         public SdaCircularBuffer(int capacity, T[] elements) {
-            throw new UnsupportedOperationException("Not implemented yet");
+            this.elements = elements;
+            size = elements.length;
+
+            if (elements.length > capacity) {
+                throw new IllegalArgumentException();
+            }
+            T[] newArrays = Arrays.copyOf(elements, capacity);
         }
 
         @Override
         public boolean isEmpty() {
-            throw new UnsupportedOperationException("Not implemented yet");
+            return elements.length == 0;
         }
 
         @Override
         public boolean isFull() {
-            throw new UnsupportedOperationException("Not implemented yet");
+            return size == elements.length;
         }
 
         @Override
         public int size() {
-            throw new UnsupportedOperationException("Not implemented yet");
+            return size;
         }
 
         @Override
         public T peek() {
+            //return elements[size];
             throw new UnsupportedOperationException("Not implemented yet");
         }
 
@@ -140,10 +208,10 @@ public class Exercises5 {
 
     /**
      * Funkcja odwraca podany stos.
-     *
+     * <p>
      * Uwaga: wolno używać jedynie metod pop(), push(), peek() i isEmpty()
      * oraz nie wolno tworzyć innych kolekcji.
-     *
+     * <p>
      * Podpowiedź: pytać o podpowiedzi :)
      */
     public <T> void reverseStack(Deque<T> stack) {
@@ -152,25 +220,25 @@ public class Exercises5 {
 
     /**
      * Funkcja oblicza podane wyrażenie, np: dla "2 * 3 + 7 * 8" zwraca 62.
-     *
+     * <p>
      * Dla ułatwienia przyjmij, że wszystkie symbole (tzn. liczby, operatory, nawiasy) są rozdzielone spacjami:
      * "( 2 + 6 ) * 10 / 8" jest poprawnym wyrażeniem
      * "(2 + 6) * 10 / 8" jest niepoprawnym wyrażeniem
-     *
+     * <p>
      * Uwaga: całą logikę najlepiej jest zaimplementować w osobnej klasie, np. Evaluator.
-     *
+     * <p>
      * Podpowiedź: ewaluacja odbywa się w dwóch krokach:
      * 1. należy użyć algorytmu shunting-yard aby przekształcić wyrażenie do notacji postfiksowej,
-     *    czyli tzn. Odwrotnej Notacji Polskiej (Reverse Polish Notation)
+     * czyli tzn. Odwrotnej Notacji Polskiej (Reverse Polish Notation)
      * 2. należy obliczyć wyrażenie zapisane w notacji postfiksowej
-     *
+     * <p>
      * Oba algorytmy są opisane na Wikipedii: https://pl.wikipedia.org/wiki/Odwrotna_notacja_polska
-     *
+     * <p>
      * W wersji podstawowej należy zaimplementować jedynie operatory +, -, * oraz /.
-     *
+     * <p>
      * W wersji bardziej zaawansowanej należy zaimplementować dodatkowo dwuargumentowe funkcje min, max, np.:
      * "4 * min ( 30 , max ( 10 , 50 ) )" zwraca 120
-     *
+     * <p>
      * Podpowiedź: wszystkie wymagane operatory (+, -, *, /) są lewostronnie łaczne.
      *
      * @throws IllegalArgumentException jeśli wyrażenie jest niepoprawne
